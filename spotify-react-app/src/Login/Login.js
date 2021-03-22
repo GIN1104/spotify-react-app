8768b127c3fecc1cd1b106d55999948bb9dd32db
Evgeny <starchenkogin@gmail.com>
2021-03-22 23:18:28 +0200

    Starting to build Player

--- spotify-react-app/src/Login/Login.js
+++ spotify-react-app/src/Login/Login.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import  './Login.css'
+import React from 'react';
+import  './Login.css';
+import { loginUrl } from '../spotify';
 
 function Login() {
     return (
         <div className="login">
             <img src="https://radio.wosu.org/sites/wosu2/files/201509/spotify-logo-horizontal-black.jpg" alt="" />
-         <a href="...">LOGIN WITH SPOTIFY</a>    
+         <a href={ loginUrl } >LOGIN WITH SPOTIFY</a>    
         </div>
     )
 }
