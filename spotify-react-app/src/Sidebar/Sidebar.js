e4befe66cb4c93c800a41015e30d1f2726a075c0
Evgeny <starchenkogin@gmail.com>
2021-03-24 11:59:32 +0200

    started with footer

--- spotify-react-app/src/Sidebar/Sidebar.js
+++ spotify-react-app/src/Sidebar/Sidebar.js
@@ -7,7 +7,7 @@ import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import { useDataLayerValue } from '../DataLayer';
 
 function Sidebar() {
-  const [{playlists}, dispatch] = useDataLayerValue();
+  const [{playlists}] = useDataLayerValue();
     return (
         <div className="sidebar">
           <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
